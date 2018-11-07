ba68826790b1191e2d6cce044230f41dea18ad04
Ex <2462148389@qq.com>
2018-11-07 19:59:00 +0800

    add some new function

--- client.py
+++ client.py
@@ -3,10 +3,25 @@
 
 import socket
 import time
-from pynput import keyboard
+from pynput import keyboard,mouse
 from pynput.keyboard import Key, Controller
-import json
 k=Controller()
+
+from pynput.mouse import Button,Controller
+m=Controller()
+
+import json
+import _thread
+
+from pyperclip import *
+
+default_x=800
+default_y=800
+
+scroll_times=40
+
+SOCKET_MAX=60000
+SEGMENT=10000
 
 #for exit
 end=Key.f3
@@ -21,14 +36,13 @@ port = 9999
 s.connect(("192.168.3.30", port))
 
 
-
 def on_press(key):
 
     if (key==end):
         data = {"type": "close"}
 
-        msg = json.dumps(data)
-        print(time.strftime("%H:%M:%S    ",time.localtime())+msg)
+        msg = json.dumps(data)+','
+        print(time.strftime("    %H:%M:%S    ",time.localtime())+msg)
         msg=msg.encode('utf-8')
         s.send(msg)
         s.close()
@@ -39,7 +53,7 @@ def on_press(key):
         
         data['key']=str(key).replace("'","")
 
-        msg = json.dumps(data)
+        msg = json.dumps(data)+','
         print(time.strftime("    %H:%M:%S    ",time.localtime())+msg)
         msg=msg.encode('utf-8')
         s.send(msg)
@@ -54,13 +68,135 @@ def on_release(key):
 
     data['key']=str(key).replace("'","")
 
-    msg = json.dumps(data)
+    msg = json.dumps(data)+','
+    print(time.strftime("    %H:%M:%S    ",time.localtime())+msg)
+    msg=msg.encode('utf-8')
+    s.send(msg)
+
+def on_move(x, y):
+    data = {'type': 'mouse', 'method': 'move'}
+    x=x-default_x
+    y=y-default_y
+
+    if(x==0 and y==0):
+        return
+
+    data['x']=x
+    data['y']=y
+
+    m.position=(default_x,default_y)
+
+    msg = json.dumps(data)+','
     print(time.strftime("    %H:%M:%S    ",time.localtime())+msg)
     msg=msg.encode('utf-8')
     s.send(msg)
 
 
-with keyboard.Listener(
-    on_press = on_press,
-    on_release = on_release) as listener:
-    listener.join()
+def on_click(x, y, button, method):
+    data = {'type': 'mouse'}
+
+    if (method):
+        data['method']="press"
+    else:
+        data['method']="release"
+
+    data['key']=str(button)
+
+    msg = json.dumps(data)+','
+    print(time.strftime("    %H:%M:%S    ",time.localtime())+msg)
+    msg=msg.encode('utf-8')
+    s.send(msg)
+
+def on_scroll(x, y, dx, dy):
+    data = {'type': 'mouse','method':'scroll'}
+
+    data['dx']=int(dx)  * scroll_times
+    data['dy']=int(dy)  * scroll_times
+    print(str(int(dx))+'\t'+str(dy))
+
+    msg = json.dumps(data)+','
+    print(time.strftime("    %H:%M:%S    ",time.localtime())+msg)
+    msg=msg.encode('utf-8')
+    s.send(msg)
+
+def listen_mouse():
+    with mouse.Listener(
+            on_move=on_move,
+            on_click=on_click,
+            on_scroll=on_scroll) as listener:
+        listener.join()
+
+
+
+def wait_clipboard():
+    new=''
+    while (True):
+        msg=s.recv(SOCKET_MAX).decode('utf-8')
+        print(time.strftime("    %H:%M:%S    ",time.localtime())+msg)
+        data = json.loads(msg[:-1])
+
+        if(data['type']=='clipboard'):
+            if(data['is_end']=='false'):
+                new+=data['content']
+            elif(data['is_end']=='true'):
+                new+=data['content']
+
+                copy(new)
+                new=''
+
+            else:
+                print('wait_clipboard Error')
+                break
+
+
+
+
+def main():
+    m.position=(default_x,default_y)
+
+    with keyboard.Listener(
+        on_press = on_press,
+        on_release = on_release) as listener:
+
+
+        listener.join()
+
+if(__name__=='__main__'):
+    
+    new=paste()
+
+    length=len(new)
+
+    segment=0
+    if(length%SEGMENT==0):
+        segment=int(length/SEGMENT)
+    else:
+        segment=int(length/SEGMENT)+1
+
+    for i in range(segment):
+
+        data = {'type': 'clipboard'}
+        if(i+1==segment):
+            data['is_end']='true'
+        
+            data['content']=new[SEGMENT*i:SEGMENT*(i+1)]
+        else:
+            data['is_end']='false'
+        
+            data['content']=new[SEGMENT*i:]
+
+        msg = json.dumps(data)+','
+        print(time.strftime("    %H:%M:%S    ",time.localtime())+msg)
+        msg=msg.encode('utf-8')
+        s.send(msg)
+
+        time.sleep(0.01)
+
+
+    try:
+        _thread.start_new_thread( listen_mouse, ( ) )
+        _thread.start_new_thread( wait_clipboard, ( ) )
+    except:
+        print ("Error: 无法启动线程")
+
+    main()
--- server.py
+++ server.py
@@ -2,13 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from pynput import keyboard
-from pynput.keyboard import Key, Controller,KeyCode
+from pynput.keyboard import Key, Controller
+k = Controller()
+
+from pynput.mouse import Button,Controller
+m=Controller()
+
 import socket
 import json
 import re
 import time
+import _thread
 
-k = Controller()
+from pyperclip import *
+
+SOCKET_MAX=60000
+SEGMENT=10000
 
 # 创建 socket 对象
 serversocket = socket.socket(
@@ -44,6 +53,17 @@ table={
 "Key.enter":Key.enter,
 "Key.esc":Key.esc,
 "Key.f1":Key.f1,
+"Key.f2":Key.f2,
+"Key.f3":Key.f3,
+"Key.f4":Key.f4,
+"Key.f5":Key.f5,
+"Key.f6":Key.f6,
+"Key.f7":Key.f7,
+"Key.f8":Key.f8,
+"Key.f9":Key.f9,
+"Key.f10":Key.f10,
+"Key.f11":Key.f11,
+"Key.f12":Key.f12,
 "Key.home":Key.home,
 "Key.insert":Key.insert,
 "Key.left":Key.left,
@@ -60,48 +80,204 @@ table={
 "Key.shift_r":Key.shift_r,
 "Key.space":Key.space,
 "Key.tab":Key.tab,
-"Key.up":Key.up
+"Key.up":Key.up,
+"A":"a",
+"B":"b",
+"C":"c",
+"D":"d",
+"E":"e",
+"F":"f",
+"G":"g",
+"H":"h",
+"I":"i",
+"J":"j",
+"K":"k",
+"L":"l",
+"M":"m",
+"N":"n",
+"O":"o",
+"P":"p",
+"Q":"q",
+"R":"r",
+"S":"s",
+"T":"t",
+"U":"u",
+"V":"v",
+"W":"w",
+"X":"x",
+"Y":"y",
+"Z":"z",
+")":"0",
+"!":"1",
+"@":"2",
+"#":"3",
+"$":"4",
+"%":"5",
+"^":"6",
+"&":"7",
+"*":"8",
+"(":"9",
+"_":"-",
+"+":"=",
+"{":"[",
+"}":"]",
+"|":"\\",
+":":";",
+"'":"\"",
+"<":",",
+">":".",
+"?":"/"
 }
-end=0
+
+mouse_table={
+    'Button.left':Button.left,
+    'Button.right':Button.right
+}
+
+def key_model(data):
+
+    key=''
+    if data['key'] in table.keys():
+        key=table[data['key']]
+    else:
+        key=data['key']
+
+    if(data['method']=='press'):
+        k.press(key)
+    elif(data['method']=='release'):
+        k.release(key)
+    else:
+        print('key-Unknow')
+        
+        return False
+
+def mouse_model(data):
+
+    if(data['method']=='move'):
+        x=data['x']
+        y=data['y']
+        m.move(x,y)
+    elif(data['method']=='press'):
+        m.press(mouse_table[data['key']])
+
+    elif(data['method']=='release'):
+        m.release(mouse_table[data['key']])
+
+    elif(data['method']=='scroll'):
+        dx=data['dx']
+        dy=data['dy']
+        m.scroll(dx,dy)
+
+    else:
+        print('mouse-Unknow')
+        
+        return False
+new=''
+def wait_clipboard(data):
+    global new
+
+    if(data['is_end']=='false'):
+        new+=data['content']
+    elif(data['is_end']=='true'):
+        new+=data['content']
+
+        copy(new)
+        new=''
+
+    else:
+        print('wait_clipboard Error')
+        return False
+            
+
+def data_analysis(msgs):
+    datas = json.loads(msgs)
+
+    for data in datas:
+        if(data['type'] == 'close'):
+            return False
+
+        elif(data['type']=='key'):
+            if(key_model(data)==False):
+                return False
+
+        elif(data['type']=='mouse'):
+            if(mouse_model(data)==False):
+                return False
+
+        elif(data['type']=='clipboard'):
+            if(wait_clipboard(data)==False):
+                return False
+
+
+        else:
+            print('type-Unknow')
+            
+            return False
+
+s_open=False
+def listen_clipboard(s):
+    time.sleep(1)
+    print("Thread start")
+    old=paste()
+    while (s_open):
+        
+
+        new=paste()
+        if(new!=old and new!=''):
+            old=new
+
+            length = len(new)
+
+            segment=0
+            if(length%SEGMENT==0):
+                segment=int(length/SEGMENT)
+            else:
+                segment=int(length/SEGMENT)+1
+
+            for i in range(segment):
+
+                data = {'type': 'clipboard'}
+                if(i+1==segment):
+                    data['is_end']='true'
+               
+                    data['content']=new[SEGMENT*i:SEGMENT*(i+1)]
+                else:
+                    data['is_end']='false'
+               
+                    data['content']=new[SEGMENT*i:]
+
+                msg = json.dumps(data)+','
+                print(time.strftime("%H:%M:%S    ",time.localtime())+msg)
+                msg=msg.encode('utf-8')
+                s.send(msg)
+
+                time.sleep(0.01)
+
+        time.sleep(1)
+
+    print("Thread over")
+
+
 while (True):
     # 建立客户端连接
     clientsocket, addr = serversocket.accept()
 
+    s_open=True
+
+    try:
+        _thread.start_new_thread( listen_clipboard, (clientsocket, ) )
+    except:
+        print ("Error: 无法启动线程")
+    
+
     while (True):
-        if(end==1):
-            end=0
+
+        msgs = clientsocket.recv(SOCKET_MAX).decode('utf-8')
+        print(time.strftime("%H:%M:%S    ",time.localtime())+msg)
+        msgs='['+msgs[:-1]+']'
+        if(data_analysis(msgs)==False):
+            clientsocket.close()
             break
 
-        msgs = clientsocket.recv(1024).decode('utf-8')
-        print(msgs)
-        msgs = re.findall('\{[\d\D]+?\}(?!=\")', msgs)
-
-        for msg in msgs:
-            if(msg==''):
-                break
-
-            print(time.strftime("%H:%M:%S    ",time.localtime())+msg)
-        
-            data=json.loads(msg)
-
-            if(data['type'] == 'close'):
-                clientsocket.close()
-                end=1
-                break
-
-            elif(data['type']=='key'):
-                key=''
-                if data['key'] in table.keys():
-                    key=table[data['key']]
-                else:
-                    key=data['key']
-
-                if(data['method']=='press'):
-                    k.press(key)
-                elif(data['method']=='release'):
-                    k.release(key)
-                else:
-                    print('Unknow')
-                    clientsocket.close()
-                    break
+    s_open=False
 
